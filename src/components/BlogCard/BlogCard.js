f18e964f3f06c4542370bdbe3f0c8b67d5c1bca0
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-06 15:48:46 -0800

    added author to blog card

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './BlogCard.css';
 
-export default function BlogCard({ title, subtitle, text, image }) {
+export default function BlogCard({ title, subtitle, text, image, authors: { name } }) {
   return (
     <div className="blog-card">
       <div className="details">
         <div className="title">{title}</div>
         <div className="subtitle">{subtitle}</div>
         <div className="text">{text}</div>
+        <div>{name}</div>
       </div>
       <div className="image">
         <img src={image} />
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,7 +1,7 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
-  const response = await client.from('blogs').select();
+  const response = await client.from('blogs').select(`*, authors(name)`);
   console.log(response);
   return checkError(response);
 }
