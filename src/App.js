1f726413b098430a1bc08eefcb19b7a20de1987c
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-06 15:18:23 -0800

    rendered blogcard by mapping blog data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
-// import { getBlogs } from './services/blogs';
+import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getBlogs();
+      setBlogs(data);
+    };
+    fetchData();
+  }, []);
   return (
     <section className="main">
       <Header />
+      {blogs.map((blogs) => {
+        return <BlogCard key={blogs.id} {...blogs} />;
+      })}
+
       <Footer />
     </section>
   );
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,7 @@
 import { checkError, client } from './client';
 
 export async function getBlogs() {
-  const response = await client.from('restaurants').select();
+  const response = await client.from('blogs').select();
+  console.log(response);
   return checkError(response);
 }
